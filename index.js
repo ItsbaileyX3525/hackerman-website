f600ea7dc1136e63552a5fa597e47b8af6597481
ItsbaileyX <baileyleah542@gmail.com>
2025-06-25 16:43:47 +0100

    update some ports

--- index.js
+++ index.js
@@ -236,8 +236,8 @@ app.post('/submit', (req, res) => {
 
 app.use('/api', submitRoutes);
 
-const port = process.env.PORT || 3000;
+const port = 80;
 
-const server = app.listen(port, () => {
-	console.log(`Server running on http://localhost:${port}`);
+const server = app.listen(port, '0.0.0.0', () => { 
+	console.log(`Server running on http://0.0.0.0:${port}`);
 });
